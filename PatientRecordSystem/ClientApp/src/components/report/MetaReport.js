2765b0d9399d558c9d8f9d8deedf2de34aae17eb
Calin Dumitru Cerghizan <Calin.Dumitru.Cerghizan@bd.com>
2020-07-09 10:39:51 +0200

    Refactor MetaReport component

--- PatientRecordSystem/ClientApp/src/components/report/MetaReport.js
+++ PatientRecordSystem/ClientApp/src/components/report/MetaReport.js
@@ -1,15 +1,14 @@
 ﻿import React, { Component } from 'react';
-import { ListGroup, Badge  } from 'react-bootstrap';
+import ReactTable from 'react-table-6';
+import 'react-table-6/react-table.css';
+import { ListGroup } from 'react-bootstrap';
 
 export class MetaReport extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            metaReport: '',
-            first: '',
-            second: '',
-            third: ''
+            metaReport: ''
         }
     }
 
@@ -19,39 +18,39 @@ export class MetaReport extends Component {
             {
                 method: "GET"
             }).then(response => response.json()).then(metaReport => {
-                const first = metaReport.topThreeUsedKeys[0];
-                const second = metaReport.topThreeUsedKeys[1];
-                const third = metaReport.topThreeUsedKeys[2];
-                this.setState({ metaReport: metaReport, first: first, second: second, third: third });
+                console.log(metaReport);
+                this.setState({ metaReport: metaReport });
         });
     }
 
     render() {
+
+        const columns = [
+            {
+                Header: 'Key',
+                accessor: 'key'
+            },
+            {
+                Header: 'Occurrence',
+                accessor: 'occurrence'
+            }
+        ];
+
         return (
             <div>
             <h3>MetaData Report</h3> 
             <ListGroup>
                     <ListGroup.Item>
-                        Average number of meta-data used <Badge pill variant="primary">{this.state.metaReport.metaUsedAverage}</Badge>
+                        Average number of meta-data used <h5>{this.state.metaReport.metaUsedAverage}</h5>
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        Maximum number of meta-data used <Badge pill variant="primary">{this.state.metaReport.metaUsedMax}</Badge>
+                        Maximum number of meta-data used <h5>{this.state.metaReport.metaUsedMax}</h5>
                     </ListGroup.Item> 
                     <ListGroup.Item>
                         Top 3 highest keys used
-                        <ListGroup>
-                            <ListGroup.Item>
-                                Key <Badge variant="success">{this.state.first.key}</Badge> has an occurrence of <Badge pill variant="primary">{this.state.first.occurrence}</Badge>
-                            </ListGroup.Item> 
-                            <ListGroup.Item>
-                                Key <Badge variant="success">{this.state.second.key}</Badge> has an occurrence of <Badge pill variant="primary">{this.state.second.occurrence}</Badge>
-                            </ListGroup.Item> 
-                            <ListGroup.Item>
-                                Key <Badge variant="success">{this.state.third.key}</Badge> has an occurrence of <Badge pill variant="primary">{this.state.third.occurrence}</Badge>
-                            </ListGroup.Item> 
-                        </ListGroup> 
+                        <ReactTable data={this.state.metaReport.topThreeUsedKeys} columns={columns} defaultPageSize={3} />
                     </ListGroup.Item> 
-                </ListGroup> 
+            </ListGroup> 
             </div>
     )
     }
